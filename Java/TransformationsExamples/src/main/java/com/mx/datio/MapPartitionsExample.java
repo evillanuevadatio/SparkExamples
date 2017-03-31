175b3fda1854a17ba409c314e6381ee4990577a0
Eder <evillanuevadatio@datiobd.com>
2017-03-31 14:19:51 -0600

    Correccion del ejemplo de MapPartitions

--- Java/TransformationsExamples/src/main/java/com/mx/datio/MapPartitionsExample.java
+++ Java/TransformationsExamples/src/main/java/com/mx/datio/MapPartitionsExample.java
@@ -4,8 +4,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by evillanueva on 29/03/17.
@@ -13,23 +17,46 @@ import java.util.Iterator;
 public class MapPartitionsExample {
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: MapExample-1.0-SNAPSHOT.jar [input] [output]");
+        if (args.length < 1) {
+            System.out.println("Usage: MapExample-1.0-SNAPSHOT.jar [output]");
             System.exit(1);
         } else {
-            String input = args[0];
-            String output = args[1];
+            String output = args[0];
+
 
             JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("MaxPriceJava"));
-            JavaRDD<String> inputFile = sc.textFile(input);
+            //JavaRDD<String> inputFile = sc.textFile(input);
 
-            inputFile.mapPartitions(
-                    new FlatMapFunction<Iterator<String>, Object>() {
-                        public Iterator<Object> call(Iterator<String> stringIterator) throws Exception {
-                            return null;
+            JavaRDD<Integer> rdd = sc.parallelize( Arrays.asList(1, 2, 3, 4, 5,6,7,8,9,10,11,12),3);
+
+
+            JavaRDD<Integer> integerJavaRDD = rdd.mapPartitions(
+                    new FlatMapFunction<Iterator<Integer>, Integer>() {
+                        public Iterator<Integer> call(Iterator<Integer> intIter) throws Exception {
+                            /*
+                            Integer acum = 0;
+
+                            List<Integer> ap1 = new ArrayList<Integer>();
+                            while (intIter.hasNext()) {
+                                acum += intIter.next();
+                                ap1.add(acum);
+                            }
+                            return ap1.iterator();
+                            */
+                            return intIter;
                         }
                     }
             );
+
+            //integerJavaRDD.saveAsTextFile(output+"MapPartitions");
+            Integer x = integerJavaRDD.reduce(
+                    new Function2<Integer, Integer, Integer>() {
+                        public Integer call(Integer integer, Integer integer2) throws Exception {
+                            return integer+integer2;
+                        }
+                    }
+            );
+            System.out.println("Reduced x: " + x);
 
         }
     }
